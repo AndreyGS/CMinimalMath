ee5adce09886da7a56a67e338b94204fb3b4bab0
AndreyGS <ukbpyh@gmail.com>
2020-06-09 23:59:44 +0300

    get_sldouble_fromd was simplified to have better readability. get_double_ieee754 had get the few insignificant syntax updates.

--- src/sldouble.c
+++ src/sldouble.c
@@ -46,7 +46,7 @@ sldouble get_sldouble_fromd(const double d)
 {
     sldouble sd;    
 
-    char *restrict p = (char *) &d, c;
+    char *restrict p = (char *) &d;
     
     /* We are using this way to find out what the leading bit
      * status instead of simply comparison of double number with zero 
@@ -56,56 +56,29 @@ sldouble get_sldouble_fromd(const double d)
 
     /* Left offset for uint64_t raw fetching, which is default to zero
      * in case of normal (not denormal) numbers */
-    int i = 0;
+    unsigned int i = 0;
     /* In the begining, we getting exponent range (with part of mantissa catching) */
     /* Next tricky statement we could split to:
      *      int16_t *e16 = (int16_t *) ps + 3;
      *      int64_t e = (int64_t) ((*e16 & 0x7ff0) >> 4) - 1023; */
     int64_t e = (int64_t) ((*((int16_t *) p + 3) & 0x7ff0) >> 4) - 1023;
-
+    
+    /* Cases of NAN and +-INF */
     if (e == 1024) {
         if (d != d) set_special_nan(&sd)
         else set_special_inf(&sd, d)
         return sd;
+    /* Cases of zeros and denormal numbers */
     } else if (e == -1023) {
         if (d == 0.0) {
             set_special_zero(&sd, 0.0, sd._nsign)
             return sd; 
         }
-        /* If we've got denormal numbers on input, we need to
-         * get a leftmost zeros preceding too, to compute final
-         * exponent value and to get second bordrer for raw variable fetching,
-         * which is default in case of normal nombers */
-        p += 6;
-        c = *p;
-        /* i already is 0, so we need head condition check
-         * only for final 'else' working properly */
-        if      (c & 0x08)      ;
-        else if (c & 0x04) i = 1;
-        else if (c & 0x02) i = 2;
-        else if (c & 0x01) i = 3;
-        else i = 4;
         
-        if (i == 4) {
-            /* As it is not zero (as we checked in the beginning
-             * of the function, at least last byte must be not equal to 0
-             * so the resulting edges in cycle condition operators 
-             * are not include such possibilities */
-            while(*--p == 0) i += 8;
-
-            c = *p;
-            if      (c & 0x80)       ;
-            else if (c & 0x40) ++i   ;
-            else if (c & 0x20) i += 2;
-            else if (c & 0x10) i += 3;
-            else if (c & 0x08) i += 4;
-            else if (c & 0x04) i += 5;
-            else if (c & 0x02) i += 6;
-            else               i += 7;
-        }
-        e -= i;
-        
-        p = (char *) &d;
+        /* Here i is substracting 11 but not 12 because of later
+         * calculating of sd._len, and i must be bigger on 1 there */
+        i = get_number_of_leading_zeros_64bit_var(p) - 11;
+        e -= i-1;
     }
     
     sd._dbl = d;
@@ -113,42 +86,15 @@ sldouble get_sldouble_fromd(const double d)
     sd._exp = e;
     
     /* Right offset for uint64_t raw fetching */
-    int j = 0;
-    /* To get the raw bits value, we need to do some tricky manipulations */
-    /* First we need to calculate trailing zeros number */
-    /* Here is no need to upperbound checking, if it is not 0 or some of other
-     * special cases (and it is not, as we checked it in the begining 
-     * of the function), than there must be at least one 1 in mantissa */
-    --p;
-    while(*++p == 0) j+=8;
-    
-    c = *p;
-    if (j >= 48) {
-        if      (c & 0x01) j = 48;
-        else if (c & 0x02) j = 49;
-        else if (c & 0x04) j = 50;
-        else if (c & 0x08) j = 51;
-        else               j = 52;
-    } else {
-        if      (c & 0x01)       ;
-        else if (c & 0x02) ++j   ;
-        else if (c & 0x04) j += 2;
-        else if (c & 0x08) j += 3;
-        else if (c & 0x10) j += 4;
-        else if (c & 0x20) j += 5;
-        else if (c & 0x40) j += 6;
-        else               j += 7;
-    }
-    
-    p = (char *) &d;
-    /* And the second - transform current value to integer raw */
-    /* this statement can be expressed simpler by:
+    unsigned int j = get_number_of_trailing_zeros_64bit_var(p);
+    if (j > 52) j = 52;
+
+    /* This statement can be expressed simpler by:
      * uint64_t r = *((uint64_t *) p); r = (r & 0x000fffffffffffff) >> j; */
     uint64_t r = (*((uint64_t *) p) & 0x000fffffffffffff) >> j;
     sd._len = 53 - i - j;
     /* If we've got normal numbers we need to add leading '1' */
     if (e > -1022) r |= 0x0010000000000000 >> j;
-    else --sd._len;
 
     sd._raw = r;
 
@@ -342,15 +288,14 @@ double get_double_ieee754(sldouble *restrict const sd)
                 ++sd->_exp;
                 if (exp > 0) ++addoffset;
             }
-            raw += 1;
+            ++raw;
         }
         raw >>= addoffset;
+        sd->_raw = raw >> get_number_of_trailing_zeros_64bit_var(&raw);
     } else {
         raw <<= leftmost_fraction_significant_bit - len;
     }
 
-    sd->_raw = raw >> get_number_of_trailing_zeros_64bit_var(&raw);
-   
     /* Clearing leading bit and adding exp value in raw of normal number */
     if (exp > 0) {
         raw &= 0x000fffffffffffff;
@@ -359,14 +304,14 @@ double get_double_ieee754(sldouble *restrict const sd)
 
     if (sd->_nsign) raw |= 0x8000000000000000;
 
-    /* Compiler put a warning of strict aliasing on -O2 and -O3 
-    * optimization levels when we directly cast vars 
-    * if we did like this:
-    *
-    * return sd->_dbl = *(double *) &raw;
-    *
-    * So to not broke opmization algorithms
-    * we need to add an intermidiate  step. */
+    /* Compiler put a warning about strict aliasing on -O2 and -O3 
+     * optimization levels when we directly cast vars 
+     * if we do like this:
+     *
+     * return sd->_dbl = *(double *) &raw;
+     *
+     * So to not broke opmization algorithms
+     * we need to add an intermidiate  step. */
     const char *restrict const p = (const char *) &raw;
     sd->_flags |= HASDOUBLE;
     return sd->_dbl = *(double *) p;
